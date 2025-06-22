3d448f6bca0036ab109ff3568250c699d60e7993
Maxwell <kevinmaxwell205@gmail.com>
2025-06-23 05:01:40 +0700

    Configure environment variables for production API

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: '/api', // Menggunakan path proxy
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export const getIdeas = (params) => {
